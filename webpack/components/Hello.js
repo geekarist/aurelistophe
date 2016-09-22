5a7fa0f9a7b5861edec9f1bacd356686c695657e
cpcp <christophe.pele@gmail.com>
2016-09-22 17:04:14 +0200

    Setup firebase

--- webpack/components/Hello.js
+++ webpack/components/Hello.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+
 class Hello extends Component {
+
     render() {
         return (
             <div>Hello!</div>
         );
     }
 }
+
 export default Hello;
--- webpack/entry.js
+++ webpack/entry.js
@@ -1,11 +1,27 @@
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/database");
+
+var config = {
+    apiKey: "AIzaSyDfPuxNav3inT1JeWvzWRQk-AHvdCwpqqw",
+    authDomain: "aurelistophe.firebaseapp.com",
+    databaseURL: "https://aurelistophe.firebaseio.com",
+    storageBucket: "aurelistophe.appspot.com",
+    messagingSenderId: "164238200047"
+};
+firebase.initializeApp(config);
+
 import React, { Component } from 'react';
 import {render} from 'react-dom';
 import Hello from './components/Hello';
+
 class App extends Component {
+
     render() {
         return (
             <Hello />
         )
     }
 }
+
 render(<App />, document.getElementById('root'));
